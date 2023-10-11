6489e8b491eac2665c5aaac02a1415d1c1c4ee0a
thi4go-git <thi4go19@gmail.com>
2023-10-11 14:41:01 -0300

    OpenApi Swagger

---
+++ src/main/java/br/com/alvara/configuration/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package br.com.alvara.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Alvarás WEB")
+                        .description("API Para manipular Alvarás")
+                        .contact(new Contact()
+                                .name("Thiago Junior")
+                                .email("thi4go19@gmail.com")
+                                .url("https://www.linkedin.com/in/thiago-amorim-melo/")
+                        )
+                        .version("11/10/2023"));
+    }
+
+}
