825f7b5d0b1500e639206be70a24356dddd6fa50
thi4go-git <thi4go19@gmail.com>
2023-11-03 20:05:28 -0300

    Versão 11/2023

--- src/main/java/br/com/alvara/configuration/OpenApiConfig.java
+++ src/main/java/br/com/alvara/configuration/OpenApiConfig.java
@@ -20,7 +20,7 @@ public class OpenApiConfig {
                                 .email("thi4go19@gmail.com")
                                 .url("https://www.linkedin.com/in/thiago-amorim-melo/")
                         )
-                        .version("16/10/2023"));
+                        .version("03/11/2023"));
     }
 
 }
