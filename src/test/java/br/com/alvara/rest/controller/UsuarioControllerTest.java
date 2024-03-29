f01629b8cda3698577fe1db1c77119839ddca8d0
Thiago junior <thi4go19@gmail.com>
2024-03-28 22:13:33 -0300

    Teste parametrizado para cadastro de usu[ário

--- src/test/java/br/com/alvara/rest/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/alvara/rest/controller/UsuarioControllerTest.java
@@ -7,6 +7,10 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +20,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -38,13 +43,16 @@ class UsuarioControllerTest {
     private static final Logger LOG = LoggerFactory.getLogger(UsuarioControllerTest.class);
     private static final String URL_PATH = "/api/usuarios";
     private static final String ID_USUARIO = "1";
+    private static final String ID_USUARIO_INEXISTENTE = "4";
     private static final String NOME_FOTO_MOCK = "foto";
+    private static final String MSG_USER_NOTFOUND = "Usuário não encontrado com o ID informado!";
 
-    @Test
-    @DisplayName("Deve salvar Usuario com Sucesso!")
+
+    @ParameterizedTest
+    @MethodSource("usuariosParaTeste")
+    @DisplayName("Testa Cadastro de usuários")
     @Order(1)
-    void salvarUsuarioSucesso() {
-        UsuarioDTO dto = gerarUsuarioDTO();
+    void testarCadastroDeUsuarios(UsuarioDTO dto, int indexUsuario) {
         var resposta = given()
                 .contentType(ContentType.JSON)
                 .body(dto)
@@ -56,65 +64,36 @@ class UsuarioControllerTest {
         String responseBody = resposta.getBody().asString();
         LOG.info(responseBody);
         assertNotNull(responseBody);
-        assertEquals(HttpStatus.SC_CREATED, resposta.statusCode());
-        assertEquals(ID_USUARIO, resposta.jsonPath().getString("id"));
-        assertEquals(dto.getUsername(), resposta.jsonPath().getString("username"));
-        assertEquals(dto.getPassword(), resposta.jsonPath().getString("password"));
-        assertEquals("USER", resposta.jsonPath().getString("role"));
-        assertEquals("false", resposta.jsonPath().getString("ativo"));
-        assertEquals(dto.getNome(), resposta.jsonPath().getString("nome"));
-        assertEquals(dto.getCpf(), resposta.jsonPath().getString("cpf"));
-        assertEquals(dto.getEmail(), resposta.jsonPath().getString("email"));
-        assertEquals(dto.getCelular(), resposta.jsonPath().getString("celular"));
-    }
 
-    @Test
-    @DisplayName("Não Deve salvar Usuario com CPF e Username já cadastrado!")
-    @Order(2)
-    void naoSalvarUsuarioComCpfUsernameJaCadastrado() {
-        UsuarioDTO dto = gerarUsuarioDTO();
-        var resposta = given()
-                .contentType(ContentType.JSON)
-                .body(dto)
-                .when()
-                .post(URL_PATH.concat("/novo"))
-                .then()
-                .extract()
-                .response();
-        String responseBody = resposta.getBody().asString();
-        LOG.info(responseBody);
-        assertNotNull(responseBody);
-        assertEquals(HttpStatus.SC_BAD_REQUEST, resposta.statusCode());
-        assertTrue(responseBody.contains("Já existe um Usuário cadastrado com o CPF informado"));
-        assertTrue(responseBody.contains("Já existe um Usuário cadastrado com o Username informado"));
-    }
-
-    @Test
-    @DisplayName("Não Deve salvar Usuario com dados incompletos!")
-    @Order(3)
-    void naoSalvarUsuarioComDadosIncompletos() {
-        UsuarioDTO dto = new UsuarioDTO();
-        var resposta = given()
-                .contentType(ContentType.JSON)
-                .body(dto)
-                .when()
-                .post(URL_PATH.concat("/novo"))
-                .then()
-                .extract()
-                .response();
-        String responseBody = resposta.getBody().asString();
-        LOG.info(responseBody);
-        assertNotNull(responseBody);
-        assertEquals(HttpStatus.SC_BAD_REQUEST, resposta.statusCode());
-        assertTrue(responseBody.contains("O campo cpf é Obrigatório!"));
-        assertTrue(responseBody.contains("O campo nome é Obrigatório!"));
-        assertTrue(responseBody.contains("O campo username é Obrigatório!"));
-        assertTrue(responseBody.contains("O campo password é Obrigatório!"));
+        if (indexUsuario == 1) {
+            assertEquals(HttpStatus.SC_CREATED, resposta.statusCode());
+            assertEquals(ID_USUARIO, resposta.jsonPath().getString("id"));
+            assertEquals(dto.getUsername(), resposta.jsonPath().getString("username"));
+            assertEquals(dto.getPassword(), resposta.jsonPath().getString("password"));
+            assertEquals("USER", resposta.jsonPath().getString("role"));
+            assertEquals("false", resposta.jsonPath().getString("ativo"));
+            assertEquals(dto.getNome(), resposta.jsonPath().getString("nome"));
+            assertEquals(dto.getCpf(), resposta.jsonPath().getString("cpf"));
+            assertEquals(dto.getEmail(), resposta.jsonPath().getString("email"));
+            assertEquals(dto.getCelular(), resposta.jsonPath().getString("celular"));
+        } else if (indexUsuario == 2) {
+            assertEquals(HttpStatus.SC_BAD_REQUEST, resposta.statusCode());
+            assertTrue(responseBody.contains("Já existe um Usuário cadastrado com o Username informado"));
+            assertTrue(responseBody.contains("Já existe um Usuário cadastrado com o CPF informado"));
+        } else if (indexUsuario == 3) {
+            assertEquals(HttpStatus.SC_BAD_REQUEST, resposta.statusCode());
+            assertTrue(responseBody.contains("O campo nome é Obrigatório!"));
+            assertTrue(responseBody.contains("O campo username é Obrigatório!"));
+            assertTrue(responseBody.contains("O campo password é Obrigatório!"));
+            assertTrue(responseBody.contains("O campo cpf é Obrigatório!"));
+        } else {
+            assertTrue(responseBody.contains("O campo cpf é invalido!"));
+        }
     }
 
     @Test
     @DisplayName("Deve listar todos usuários sem Parâmetros")
-    @Order(4)
+    @Order(2)
     void deveListarTodosUsuariosSemParametros() {
         var resposta = given()
                 .contentType(ContentType.JSON)
@@ -136,7 +115,7 @@ class UsuarioControllerTest {
 
     @Test
     @DisplayName("Não Deve listar usuários com parâmetros incorretos")
-    @Order(5)
+    @Order(3)
     void naoDeveListarUsuariosComParametrosIncoretos() {
         var resposta = given()
                 .contentType(ContentType.JSON)
@@ -158,51 +137,68 @@ class UsuarioControllerTest {
         assertTrue(responseBody.contains("\"numberOfElements\":0"));
     }
 
-    @Test
-    @DisplayName("Deve adicionar foto ao usuário")
-    @Order(6)
-    void deveAdicionarFotoAoUsuario() {
-        byte[] conteudoFotoMock = "Conteúdo da foto para gerar bytes mock teste".getBytes();
-        InputStream inputStream = new ByteArrayInputStream(conteudoFotoMock);
-        var resposta = given()
-                .multiPart(NOME_FOTO_MOCK, "foto.jpg", inputStream)
-                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
-                .when()
-                .put(URL_PATH.concat("/" + ID_USUARIO + "/adicionar-foto"))
-                .then()
-                .extract()
-                .response();
-        String responseBody = resposta.getBody().asString();
-        assertNotNull(responseBody);
-        assertEquals(HttpStatus.SC_OK, resposta.statusCode());
-        byte[] byteFoto = resposta.asByteArray();
-        assertTrue(byteFoto.length > 0);
-        LOG.info(responseBody);
-        LOG.info("Tamanho do array de bytes FOTO: " + byteFoto.length);
-    }
-
-    @Test
-    @DisplayName("Não Deve adicionar foto ao usuário inexistente")
-    @Order(7)
-    void naoDeveAdicionarFotoAoUsuario() {
+    @ParameterizedTest
+    @CsvSource({ID_USUARIO, ID_USUARIO_INEXISTENTE})
+    @DisplayName("Testa cadastro de foto do usuário")
+    @Order(4)
+    void testarCadastroDeFoto(String userId) {
         byte[] conteudoFotoMock = "FOTO teste para gerar bytes mock".getBytes();
         InputStream inputStream = new ByteArrayInputStream(conteudoFotoMock);
         var resposta = given()
                 .multiPart(NOME_FOTO_MOCK, "fototeste.jpg", inputStream)
                 .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
                 .when()
-                .put(URL_PATH.concat("/4/adicionar-foto"))
+                .put(URL_PATH.concat("/" + userId + "/adicionar-foto"))
                 .then()
                 .extract()
                 .response();
         String responseBody = resposta.getBody().asString();
         LOG.info(responseBody);
         assertNotNull(responseBody);
-        assertEquals(HttpStatus.SC_NOT_FOUND, resposta.statusCode());
-        assertTrue(responseBody.contains("Usuário não encontrado com o ID informado!"));
+
+        if (userId.equals(ID_USUARIO_INEXISTENTE)) {
+            assertEquals(HttpStatus.SC_NOT_FOUND, resposta.statusCode());
+            assertTrue(responseBody.contains(MSG_USER_NOTFOUND));
+        } else {
+            assertEquals(HttpStatus.SC_OK, resposta.statusCode());
+            byte[] byteFoto = resposta.asByteArray();
+            assertTrue(byteFoto.length > 0);
+            LOG.info(responseBody);
+            LOG.info("Tamanho do array de bytes FOTO: " + byteFoto.length);
+        }
+
     }
 
-    private UsuarioDTO gerarUsuarioDTO() {
+    @ParameterizedTest
+    @CsvSource({ID_USUARIO, ID_USUARIO_INEXISTENTE})
+    @DisplayName("Testa ativação do usuário")
+    @Order(5)
+    void testarAtivarDesativarUsuario(String userId) {
+        var resposta = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .patch(URL_PATH.concat("/ativar/" + userId))
+                .then()
+                .extract()
+                .response();
+        String responseBody = resposta.getBody().asString();
+        assertNotNull(responseBody);
+        if (userId.equals(ID_USUARIO_INEXISTENTE)) {
+            assertTrue(responseBody.contains(MSG_USER_NOTFOUND));
+        }
+    }
+
+
+    private static Stream<Arguments> usuariosParaTeste() {
+        return Stream.of(
+                Arguments.of(getNovoUsuario(), 1),
+                Arguments.of(getUsuarioUsernameCpfExistente(), 2),
+                Arguments.of(getUsuarioCamposInvalidos(), 3),
+                Arguments.of(getUsuarioCpfInvalido(), 4)
+        );
+    }
+
+    private static UsuarioDTO getNovoUsuario() {
         return UsuarioDTO
                 .builder()
                 .username("bernardo")
@@ -212,5 +208,19 @@ class UsuarioControllerTest {
                 .email("nelson@infouai.com")
                 .celular("8125563381")
                 .build();
+    }
+
+    private static UsuarioDTO getUsuarioUsernameCpfExistente() {
+        return getNovoUsuario();
+    }
+
+    private static UsuarioDTO getUsuarioCamposInvalidos() {
+        return new UsuarioDTO();
+    }
+
+    private static UsuarioDTO getUsuarioCpfInvalido() {
+        UsuarioDTO userCpfInvalido = new UsuarioDTO();
+        userCpfInvalido.setCpf("067451");
+        return userCpfInvalido;
     }
 }
