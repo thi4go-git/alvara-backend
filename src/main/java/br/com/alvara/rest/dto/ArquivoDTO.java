0afd1f4fee3e76670bc98cf4daa56b9f4a23a6cb
Thiago melo <thi4go19@gmail.com>
2024-04-09 11:36:40 -0300

    Testes unitários 100% ArquivoCOntroller

--- src/main/java/br/com/alvara/rest/dto/ArquivoDTO.java
+++ src/main/java/br/com/alvara/rest/dto/ArquivoDTO.java
@@ -3,29 +3,32 @@ package br.com.alvara.rest.dto;
 import br.com.alvara.model.enums.StatusDocumento;
 import br.com.alvara.model.enums.TipoDocumento;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CNPJ;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ArquivoDTO {
 
     private Integer id;
 
     private TipoDocumento tipoDoc = null;
 
-    @NotEmpty(message = "{campo.nomeArquivo.obrigatorio}")
+    @NotBlank(message = "{campo.nomeArquivo.obrigatorio}")
     private String nomeArquivo;
 
-    @NotEmpty(message = "{campo.numeroAlvara.obrigatorio}")
+    @NotBlank(message = "{campo.numeroAlvara.obrigatorio}")
     private String numeroAlvara;
 
-    @NotEmpty(message = "{campo.nomeEmpresa.obrigatorio}")
+    @NotBlank(message = "{campo.nomeEmpresa.obrigatorio}")
     private String nomeEmpresa;
 
     @CNPJ(message = "{campo.cnpjEmpresa.invalido}")
--- src/test/java/br/com/alvara/rest/controller/ArquivoControllerTest.java
+++ src/test/java/br/com/alvara/rest/controller/ArquivoControllerTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import br.com.alvara.model.enums.TipoDocumento;
 import br.com.alvara.model.repository.projection.ArquivoProjection;
+import br.com.alvara.rest.dto.ArquivoDTO;
 import br.com.alvara.rest.dto.ArquivoFilterDTO;
 import br.com.alvara.rest.dto.UsuarioDTO;
 import br.com.alvara.service.implementation.ArquivoServiceImpl;
@@ -33,7 +35,12 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -64,6 +71,8 @@ class ArquivoControllerTest {
     private static final String URL_PATH = "api/alvara";
     private static final String ID_ARQUIVO = "1";
     private static final String ID_ARQUIVO_INEXISTENTE = "4";
+    private static final String MSG_NOTFOUND = "404 NOT_FOUND";
+    private static final String MSG_ARQUIVO_NAO_ENCONTRADO = "Arquivo não localizado!";
 
 
     @Test
@@ -113,7 +122,7 @@ class ArquivoControllerTest {
         } else {
             assertNotNull(responseBody);
             assertEquals(HttpStatus.SC_NOT_FOUND, resposta.statusCode());
-            assertTrue(responseBody.contains("Arquivo não localizado!"));
+            assertTrue(responseBody.contains(MSG_ARQUIVO_NAO_ENCONTRADO));
         }
     }
 
@@ -138,6 +147,330 @@ class ArquivoControllerTest {
         assertNotNull(responseEntity);
         assertEquals("200 OK", responseEntity.getStatusCode().toString());
         assertNotNull(responseEntity.getBody());
+    }
+
+    @ParameterizedTest
+    @MethodSource("arquivosParaTeste")
+    @DisplayName("Testar atualizacao de Arquivos")
+    @Order(4)
+    void testeAtualizacaoArquivo(ArquivoDTO dto, int indexArquivo) {
+        var resposta = given()
+                .body(dto)
+                .contentType(ContentType.JSON)
+                .when()
+                .put(URL_PATH.concat("/atualizar"))
+                .then()
+                .extract()
+                .response();
+
+        String responseBody = resposta.getBody().asString();
+        LOG.info(responseBody);
+        assertNotNull(responseBody);
+
+        if (indexArquivo == 1) {
+            assertEquals(HttpStatus.SC_NO_CONTENT, resposta.statusCode());
+        } else if (indexArquivo == 2) {
+            assertEquals(HttpStatus.SC_NOT_FOUND, resposta.statusCode());
+            assertTrue(responseBody.contains(MSG_NOTFOUND));
+            assertTrue(responseBody.contains(MSG_ARQUIVO_NAO_ENCONTRADO));
+        } else if (indexArquivo == 3) {
+            assertEquals(HttpStatus.SC_BAD_REQUEST, resposta.statusCode());
+            assertTrue(responseBody.contains("campo.nomeEmpresa.obrigatorio"));
+            assertTrue(responseBody.contains("campo.numeroAlvara.obrigatorio"));
+            assertTrue(responseBody.contains("campo.nomeArquivo.obrigatorio"));
+            assertTrue(responseBody.contains("campo.cnpjEmpresa.invalido"));
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({ID_ARQUIVO, ID_ARQUIVO_INEXISTENTE})
+    @DisplayName("Testar Buscar arquivo por ID")
+    @Order(5)
+    void testarBuscarArquivoPorId(String userId) {
+        var resposta = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(URL_PATH.concat("/" + userId))
+                .then()
+                .extract()
+                .response();
+        String responseBody = resposta.getBody().asString();
+        LOG.info(responseBody);
+        assertNotNull(responseBody);
+        if (userId.equals(ID_ARQUIVO_INEXISTENTE)) {
+            assertEquals(HttpStatus.SC_NOT_FOUND, resposta.statusCode());
+            assertTrue(responseBody.contains(MSG_NOTFOUND));
+            assertTrue(responseBody.contains(MSG_ARQUIVO_NAO_ENCONTRADO));
+        } else {
+            ArquivoDTO dto = getArquivoUpdate();
+            assertEquals(HttpStatus.SC_OK, resposta.statusCode());
+            assertEquals(dto.getId().toString(), resposta.jsonPath().getString("id"));
+            assertEquals(dto.getTipoDoc().toString(), resposta.jsonPath().getString("tipoDoc"));
+            assertEquals(dto.getNomeArquivo(), resposta.jsonPath().getString("nomeArquivo"));
+            assertEquals(dto.getNumeroAlvara(), resposta.jsonPath().getString("numeroAlvara"));
+            assertEquals(dto.getNomeEmpresa(), resposta.jsonPath().getString("nomeEmpresa"));
+            assertEquals(dto.getCnpjEmpresa(), resposta.jsonPath().getString("cnpjEmpresa"));
+            assertEquals(dto.getDataVencimento().toString(), resposta.jsonPath().getString("dataVencimento"));
+            assertNull(resposta.jsonPath().getString("expira"));
+            assertNull(resposta.jsonPath().getString("observacao"));
+            assertNull(resposta.jsonPath().getString("statusDocumento"));
+            assertNull(resposta.jsonPath().getString("expira"));
+            assertEquals("QVJRVUlWTyBBdHVhbGl6YWRvIGJ5dGVzIG1vY2s=", resposta.jsonPath().getString("pdf"));
+        }
+    }
+
+    @Test
+    @DisplayName("Deve Listar Vencidos")
+    @Order(6)
+    void deveListarVencidos() {
+        Page<ArquivoProjection> pageArquivoProjectionMock = new PageImpl<>(Collections.emptyList());
+        when(arquivoService.listarVencidos(anyInt(), anyInt()))
+                .thenReturn(pageArquivoProjectionMock);
+        ResponseEntity<Page<ArquivoProjection>> responseEntity = arquivoController.listarVencidos(0, 10);
+        LOG.info(responseEntity.toString());
+        assertNotNull(responseEntity);
+        assertEquals("200 OK", responseEntity.getStatusCode().toString());
+        assertNotNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Deve Listar vencer ate 60 dias")
+    @Order(7)
+    void deveListarVencerate60dias() {
+        Page<ArquivoProjection> pageArquivoProjectionMock = new PageImpl<>(Collections.emptyList());
+        when(arquivoService.listarVencerEmAte60Dias(anyInt(), anyInt()))
+                .thenReturn(pageArquivoProjectionMock);
+        ResponseEntity<Page<ArquivoProjection>> responseEntity = arquivoController.listarVencerEmAte60Dias(0, 10);
+        LOG.info(responseEntity.toString());
+        assertNotNull(responseEntity);
+        assertEquals("200 OK", responseEntity.getStatusCode().toString());
+        assertNotNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Deve listar Documentos Sem Info")
+    @Order(8)
+    void deveListarDocumentosSemInfo() {
+        Page<ArquivoProjection> pageArquivoProjectionMock = new PageImpl<>(Collections.emptyList());
+        when(arquivoService.listarDocumentosSemInfo(anyInt(), anyInt()))
+                .thenReturn(pageArquivoProjectionMock);
+        ResponseEntity<Page<ArquivoProjection>> responseEntity = arquivoController.listarDocumentosSemInfo(0, 10);
+        LOG.info(responseEntity.toString());
+        assertNotNull(responseEntity);
+        assertEquals("200 OK", responseEntity.getStatusCode().toString());
+        assertNotNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Deve listar vencer apos 60 dias")
+    @Order(9)
+    void deveListarVencerApos60dias() {
+        Page<ArquivoProjection> pageArquivoProjectionMock = new PageImpl<>(Collections.emptyList());
+        when(arquivoService.listarVencerApos60Dias(anyInt(), anyInt()))
+                .thenReturn(pageArquivoProjectionMock);
+        ResponseEntity<Page<ArquivoProjection>> responseEntity = arquivoController.listarVencerApos60Dias(0, 10);
+        LOG.info(responseEntity.toString());
+        assertNotNull(responseEntity);
+        assertEquals("200 OK", responseEntity.getStatusCode().toString());
+        assertNotNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Deve obter total de arquivos")
+    @Order(10)
+    void deveObterTotalDeArquivos() {
+        var resposta = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(URL_PATH.concat("/totalarquivos"))
+                .then()
+                .extract()
+                .response();
+        String responseBody = resposta.getBody().asString();
+        LOG.info(responseBody);
+        assertNotNull(responseBody);
+        assertEquals(HttpStatus.SC_OK, resposta.statusCode());
+        assertEquals("1", responseBody);
+    }
+
+    @Test
+    @DisplayName("Deve obter total de arquivos vencidos")
+    @Order(11)
+    void deveObterTotalDeArquivosVencidos() {
+        when(arquivoService.totalVencidos()).thenReturn(4);
+        ResponseEntity<Integer> responseEntity = arquivoController.totalArquivosVencidos();
+        LOG.info(responseEntity.toString());
+        assertNotNull(responseEntity);
+        assertEquals("200 OK", responseEntity.getStatusCode().toString());
+        assertNotNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Deve obter total de arquivos vencer em 60 dias")
+    @Order(12)
+    void deveObterTotalDeArquivosVencerEm60Dias() {
+        when(arquivoService.totalArquivosVencerEm60Dias()).thenReturn(1);
+        ResponseEntity<Integer> responseEntity = arquivoController.totalArquivosVencerEm60Dias();
+        LOG.info(responseEntity.toString());
+        assertNotNull(responseEntity);
+        assertEquals("200 OK", responseEntity.getStatusCode().toString());
+        assertNotNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Deve obter total de arquivos sem informações")
+    @Order(13)
+    void deveObterTotalDeArquivosSemInfo() {
+        when(arquivoService.totaDocumentosSemInfo()).thenReturn(2);
+        ResponseEntity<Integer> responseEntity = arquivoController.totalArquivosSemInformacoes();
+        LOG.info(responseEntity.toString());
+        assertNotNull(responseEntity);
+        assertEquals("200 OK", responseEntity.getStatusCode().toString());
+        assertNotNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Deve obter total de arquivos vencer após 60 dias")
+    @Order(14)
+    void deveObterTotalDeArquivosVencerApos60Dias() {
+        when(arquivoService.totalArquivosVencerApos60Dias()).thenReturn(3);
+        ResponseEntity<Integer> responseEntity = arquivoController.totalArquivosVencerApos60Dias();
+        LOG.info(responseEntity.toString());
+        assertNotNull(responseEntity);
+        assertEquals("200 OK", responseEntity.getStatusCode().toString());
+        assertNotNull(responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Deve listar tipos de documento")
+    @Order(15)
+    void deveListarTiposDeDocumento() {
+        var resposta = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(URL_PATH.concat("/tipodocumento"))
+                .then()
+                .extract()
+                .response();
+        String responseBody = resposta.getBody().asString();
+        LOG.info(responseBody);
+        assertNotNull(responseBody);
+        assertEquals(HttpStatus.SC_OK, resposta.statusCode());
+        assertTrue(responseBody.contains("ALVARA_FUNCIONAMENTO"));
+        assertTrue(responseBody.contains("LICENCA_SANITARIA"));
+        assertTrue(responseBody.contains("LICENCA_AMBIENTAL"));
+        assertTrue(responseBody.contains("DESCONHECIDO"));
+        assertTrue(responseBody.contains("ALVARA_BOMBEIRO"));
+    }
+
+    @Test
+    @DisplayName("Deve listar tipos status documentos")
+    @Order(16)
+    void deveListarTiposStatusDocumento() {
+        var resposta = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(URL_PATH.concat("/status-documento"))
+                .then()
+                .extract()
+                .response();
+        String responseBody = resposta.getBody().asString();
+        LOG.info(responseBody);
+        assertNotNull(responseBody);
+        assertEquals(HttpStatus.SC_OK, resposta.statusCode());
+        assertTrue(responseBody.contains("ANALISE"));
+        assertTrue(responseBody.contains("EXIGENCIA"));
+        assertTrue(responseBody.contains("DEFERIDO"));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({ID_ARQUIVO_INEXISTENTE, ID_ARQUIVO})
+    @DisplayName("Testar deleção de arquivos")
+    @Order(17)
+    void testarDeletarArquivoPorId(String arquivoId) {
+        var resposta = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .delete(URL_PATH.concat("/delete/" + arquivoId))
+                .then()
+                .extract()
+                .response();
+        String responseBody = resposta.getBody().asString();
+        LOG.info(responseBody);
+        assertNotNull(responseBody);
+        if (arquivoId.equals(ID_ARQUIVO_INEXISTENTE)) {
+            assertEquals(HttpStatus.SC_NOT_FOUND, resposta.statusCode());
+            assertTrue(responseBody.contains(MSG_NOTFOUND));
+            assertTrue(responseBody.contains(MSG_ARQUIVO_NAO_ENCONTRADO));
+        } else {
+            assertEquals(HttpStatus.SC_NO_CONTENT, resposta.statusCode());
+        }
+    }
+
+    @Test
+    @DisplayName("Testar deleção de arquivos através de LISTA")
+    @Order(18)
+    void testarDeletarArquivoPorLista() {
+
+        List<String> listaDeletar = new ArrayList<>();
+        listaDeletar.add(ID_ARQUIVO);
+
+        var resposta = given()
+                .contentType(ContentType.JSON)
+                .body(listaDeletar)
+                .when()
+                .post(URL_PATH.concat("/deletar-multiplos"))
+                .then()
+                .extract()
+                .response();
+        String responseBody = resposta.getBody().asString();
+        LOG.info(responseBody);
+        assertNotNull(responseBody);
+        assertEquals(HttpStatus.SC_NO_CONTENT, resposta.statusCode());
+    }
+
+
+    private static Stream<Arguments> arquivosParaTeste() {
+        return Stream.of(
+                Arguments.of(getArquivoUpdate(), 1),
+                Arguments.of(getArquivoUpdateIdInexistente(), 2),
+                Arguments.of(getArquivoUpdateCamposInvalidos(), 3)
+        );
+    }
+
+    private static ArquivoDTO getArquivoUpdate() {
+        return ArquivoDTO.builder()
+                .id(1)
+                .tipoDoc(TipoDocumento.LICENCA_AMBIENTAL)
+                .nomeArquivo("nome teste")
+                .numeroAlvara("1234567890")
+                .nomeEmpresa("EMpresa LTDA")
+                .cnpjEmpresa("06139890000101")
+                .dataVencimento(LocalDate.of(2022, 1, 12))
+                .build();
+    }
+
+    private static ArquivoDTO getArquivoUpdateIdInexistente() {
+        return ArquivoDTO.builder()
+                .id(50)
+                .tipoDoc(TipoDocumento.LICENCA_AMBIENTAL)
+                .nomeArquivo("nome teste1")
+                .numeroAlvara("1234567891")
+                .nomeEmpresa("EMpresa LTDA")
+                .cnpjEmpresa("15406009000174")
+                .build();
+    }
+
+    private static ArquivoDTO getArquivoUpdateCamposInvalidos() {
+        return ArquivoDTO.builder()
+                .id(1)
+                .tipoDoc(TipoDocumento.LICENCA_AMBIENTAL)
+                .nomeArquivo("")
+                .numeroAlvara("")
+                .nomeEmpresa("")
+                .cnpjEmpresa("96139895006100")
+                .build();
     }
 
 }
