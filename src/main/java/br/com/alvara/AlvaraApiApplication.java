fedf7ce764b8b92c4ba0bef9b5b3dbdabdde16cc
Thiago Melo <thiagomelo@saude-go.net>
2025-08-06 01:50:58 -0300

    Config .env

--- src/main/java/br/com/alvara/AlvaraApiApplication.java
+++ src/main/java/br/com/alvara/AlvaraApiApplication.java
@@ -1,5 +1,6 @@
 package br.com.alvara;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class AlvaraApiApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
+        // Carregar valores do arquivo .env
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         log.info("Informação: {}", "Aplicação RODANDO!");
         SpringApplication.run(AlvaraApiApplication.class, args);
     }
